f4acf60f10aac84732890915c920de4c25a6db57
t-ube <ube@coder.okinawa.jp>
2025-05-18 13:37:39 +0900

    delete sw

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener(async (tab) => {
-  if (!tab.id || !tab.url?.startsWith('http')) return;
-
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['inject-replace-addresses.js']
-  });
-});
